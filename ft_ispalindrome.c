6a3dda265d84e5ec60b11a9c8e4fc8f2d8420dad
Krishna KC <krishnakeec@gmail.com>
2024-12-17 16:04:08 +0200

    added a test including main

--- ft_ispalindrome.c
+++ ft_ispalindrome.c
@@ -1,3 +1,6 @@
+#include <stdbool.h>
+#include <stdio.h>
+
 bool isPalindrome(int x)
 {
     int reversed;
@@ -14,4 +17,17 @@ bool isPalindrome(int x)
         return 1;
     else 
         return 0;
+}
+
+int main() 
+{
+     // List of test cases
+    int test_cases[] = {121, 12321, 0, 9999, -121, -1, 10, 100, 1000, 7, 9, 123, 56789, 45654};
+    int num_tests = sizeof(test_cases) / sizeof(test_cases[0]);
+
+    // Run and print the test results
+    for (int i = 0; i < num_tests; i++) {
+        printf("Number: %d, isPalindrome: %d\n", test_cases[i], isPalindrome(test_cases[i]));
+    }
+    return 0;
 }
